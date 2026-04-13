a23eb01b8f253355237d27b136a9e77f056ac396
maria <malubf20@gmail.com>
2026-04-13 18:15:17 -0300

    feat: Adicionando todas as páginas de routes e adicionando o texto do app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter as Browser, Routes, Route } from "react-router-dom"
+import Menu from "./components/Menu"
+import Home from "./routes/Home"
+import Contato from "./routes/Suporte"
+import Error from "./routes/Error"
+import App from "./routes/App"
+
 function App() {
 
   return (
     <>
-      
+     <Browser>
+      <Menu/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/app" element={<App />} />
+        <Route path="/suporte" element={<Suporte />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer/>
+    </Browser> 
     </>
   )
 }
