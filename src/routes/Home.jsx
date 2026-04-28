b60998dbf8c6eccde2fc96f1c051aa42132f4874
Maria_Luiza <lukochnoff@gmail.com>
2026-04-28 16:50:49 -0300

    feat: adicionando estrutura do hero

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <main>
       
-    </div>
+      <section id="hero">
+        <div className="hero-content">
+          <h1>Cuide-se hoje.<br />Floresça sempre.</h1>
+          <p>O Health Plus transforma seus hábitos diários em conquistas reais — 
+          com gamificação, recompensas e uma plantinha que cresce junto com você.
+          </p>
+          <div className="hero-buttons">
+            <a href="/AppPage">Conheça nosso Aplicativo</a>
+            <a href="/suporte">Falar com suporte</a>
+          </div>
+        </div>
+
+        <div className="hero-visual">
+
+        </div>
+        
+      </section>
+
+    </main>
   )
 }
 
